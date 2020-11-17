305184a3d1e2bc6f1ff5c4f62e37d82ae4e1ab4e
ViniciusAki <57423329+ViniciusAki@users.noreply.github.com>
2020-11-17 20:20:07 -0300

    Add files via upload

---
+++ 01-update.sh
@@ -0,0 +1,26 @@
+#!/bim/bash
+
+echo -e  "Atulizando as lista do Apt"
+
+apt update 
+
+echo -e "Atulizando as lista do sodtware"
+        apt -y upgrade
+
+echo- "Forçando a atulizaçaão do sistema"
+
+apt -y full -upgrade
+
+echo -e "Forçando a atulização de se segurança"
+apt -y dist -upgrade
+
+echo -e "Removendo softawe desnecessários"
+ 
+ apt -y autoremove
+ apt -y auto clean
+ apt clean 
+
+ echo - "Sistema atualizado, pressione <Enter> para o reboot"
+ read
+ sleep 5
+  reboot
